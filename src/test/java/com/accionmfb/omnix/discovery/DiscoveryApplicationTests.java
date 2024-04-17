49f276bc04a254a4b7099adc7803ba4a6649a066
sunday lucky enyinna <sundayluckyenyinna@gmail.com>
2024-04-17 11:18:59 +0100

    feat: omnix-discovery setup

---
+++ src/test/java/com/accionmfb/omnix/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accionmfb.omnix.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
